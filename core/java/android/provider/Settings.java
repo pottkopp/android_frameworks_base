a49dd26d8acf3fb40122e2d17d3e60944c82e275
0xD34D <clark@scheffsblend.com>
2013-03-09 10:59:45 -0700

    Frameworks: Add Permissions Management to AOKP [3/4]
    Allows the user to revoke individual permissions on a per app basis.  Adapted from
    CM7 commits 949eb057d81da7cd0a79fc1fe9cc9c4d989d1c27 and 4a0a352b36b21d0d22713c232baccca84ce13a2d
    to work with 4.2.2
    
    Change-Id: Ibc7be45b88838a2fffaff80514e77702d330de0f
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5233,6 +5233,8 @@ public final class Settings {
          */
         public static final String ADVANCED_REBOOT = "advanced_reboot";
 
+        public static final String ENABLE_PERMISSIONS_MANAGEMENT = "enable_permissions_management";
+
         /**
          * This are the settings to be backed up.
          *
