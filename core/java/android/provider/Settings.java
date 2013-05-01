80f62e9981f6067e42a71c8174a5e2ebf4ee4aca
Schubi <schubi@erlangen.ccc.de>
2013-05-01 21:59:43 +0200

    Wake On Plug
    Change-Id: I1345161fa6bb31743ed3881f68d5ba282794aab7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1974,6 +1974,13 @@ public final class Settings {
         public static final String VIBRATE_IN_SILENT = "vibrate_in_silent";
 
         /**
+         * wake up when plugged or unplugged
+         *
+         * @hide
+         */
+        public static final String WAKEUP_WHEN_PLUGGED_UNPLUGGED = "wakeup_when_plugged_unplugged";
+
+        /** 
          * The mapping of stream type (integer) to its setting.
          */
         public static final String[] VOLUME_SETTINGS = {
