12ec2fd68c7ee15aa426d8cb7b31772c9f60ff95
drcmda <drcmda@gmail.com>
2013-07-15 16:14:45 +0100

    HALO bugfixes
    1. some icons were overblown
    2. tasking meant having your thumb on the speechbubbles
    
    Change-Id: I04637a5ae96c3d9fa30188a320e13989c48f7f0c
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -1039,7 +1039,14 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
             int ch = mHaloTickerContent.getMeasuredHeight();
             int cw = mHaloTickerContent.getMeasuredWidth();
             int y = mHaloY + mIconHalfSize - ch / 2;
-            if (y < 0) y = 0;
+            if (mGesture == Gesture.TASK) {
+                if (mHaloY < mIconHalfSize) {
+                    y = y + (int)(mIconSize * 0.6f);
+                } else {
+                    y = y - (int)(mIconSize * 0.6f);
+                }
+            }
+
             int x = mHaloX + mIconSize;
             if (!mTickerLeft) {
                 x = mHaloX - cw;
@@ -1058,7 +1065,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
             // Horizontal Marker
             if (mGesture == Gesture.TASK) {
                 if (y > 0 && mNotificationData != null && mNotificationData.size() > 0) {
-                    int pulseY = (int)(mHaloY - mIconSize * 0.1f);
+                    int pulseY = mHaloY + mIconHalfSize - mMarker.getHeight() / 2;
                     int items = mNotificationData.size();
                     int indexLength = (mScreenWidth - mIconSize * 2) / items;
 
@@ -1092,7 +1099,6 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
                 yButtom = yButtom + (int)(mIconSize * 0.6f);
                 canvas.drawBitmap(mMarkerB, xPos, yButtom, mMarkerPaint);
             }
-
 
             // Bubble
             state = canvas.save();
