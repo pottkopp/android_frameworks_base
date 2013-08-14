01616f36adbecb86cbab2d58152993eced28307b
pottkopp <pottkopp7@gmail>
2013-08-14 02:12:47 +0200

    new file:   data/sounds/AudioPackageNewWave.mk

---
+++ data/sounds/AudioPackageNewWave.mk
@@ -0,0 +1,12 @@
+#
+# New Wave Audio Package
+#
+# Include this file in a product makefile to include these audio files
+#
+#
+
+LOCAL_PATH:= frameworks/base/data/sounds
+
+# Ringtones
+PRODUCT_COPY_FILES += \
+
